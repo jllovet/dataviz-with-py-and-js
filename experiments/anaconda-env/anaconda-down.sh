7b90ba5c4dccf68e83e8af40968ca84ec0de03bf
Jonathan Llovet <jonathan.llovet@gmail.com>
2019-12-12 17:16:01 -0500

    Extend dockerfile to support Jupyter notebook

--- experiments/anaconda-env/anaconda-down.sh
+++ experiments/anaconda-env/anaconda-down.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose down
-docker container stop anaconda
-docker container rm anaconda
-docker container stop anaconda-shell
-docker container rm anaconda-shell
+# docker container stop anaconda-python-3.7
+# docker container rm anaconda-python-3.7
+# docker container stop anaconda-shell
+# docker container rm anaconda-shell
--- experiments/anaconda-env/anaconda-up.sh
+++ experiments/anaconda-env/anaconda-up.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker-compose up --build
-docker run --name anaconda-shell -it anaconda
+docker-compose up --build -d >> ./logs/buildlogs.txt 2>&1
+docker-compose logs -ft >> ./logs/servicelogs.txt 2>&1 &
+# docker run --name anaconda-shell -it anaconda
