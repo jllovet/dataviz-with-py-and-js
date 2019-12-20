40d0da22d7ad011409d88a088da2165dfc7e37aa
Jonathan Llovet <jonathan.llovet@gmail.com>
2019-12-20 01:14:13 -0500

    Add containers for mysql and mongo dbs along with management interfaces

---
+++ experiments/docker/anaconda-env/db/mongo/init/mongo-init.js
@@ -0,0 +1,25 @@
+db.createCollection('nobel_winners');
+db.nobel_winners.insertMany([
+        {
+            "category": "Physics",
+            "name": "Albert Einstein",
+            "nationality": "Swiss",
+            "sex": "male",
+            "year": 1921
+        },
+        {
+            "category": "Physics",
+            "name": "Paul Dirac",
+            "nationality": "British",
+            "sex": "male",
+            "year": 1933
+        },
+        {
+            "category": "Chemistry",
+            "name": "Marie Curie",
+            "nationality": "Polish",
+            "sex": "female",
+            "year": 1911
+        }
+    ]    
+);
---
+++ experiments/docker/anaconda-env/mysql.sh
@@ -0,0 +1,2 @@
+# Runs a mysql client in the container we have created
+docker-compose run --rm mysql-client
